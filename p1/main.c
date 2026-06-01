3bf012f5860fe43d9402eb34e205e7fca7f0f4d8
Echo8485 <steve1921@kau.kr>
2026-06-01 14:59:28 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    char arr[10];
-    char *p, *q;
-    char maxChar;
-    int maxCount = 0;
+    char arr[10];//10개의 문자 저장
+    char *p, *q;// p는 기준 문자 선택용, q는 배열 전체를 탐색하며 해당 문자의 빈도수를 
+    char maxChar; //가장 많이 나올 문자 초기화
+    int maxCount = 0;//최대 빈도수 저장
 
     // 10개의 영문자 입력
-    for (p = arr; p < arr + 10; p++) {
+    for (p = arr; p < arr + 10; p++) {//p를 가리키는 문자 저장
         scanf(" %c", p);
     }
 
-    // 각 문자의 빈도수 확인
+    // p는 기준 문자를 가리키고, q는 배열 전체를 순회하며 해당 문자의 빈도수를 계산
     for (p = arr; p < arr + 10; p++) {
-        int count = 0;
+        int count = 0;// 현재 문자(*p)의 등장 횟수
 
         for (q = arr; q < arr + 10; q++) {
             if (*p == *q) {
@@ -26,6 +26,8 @@ int main(void) {
         if (count > maxCount) {
             maxCount = count;
             maxChar = *p;
+        // count == maxCount인 경우는 무시하고 먼저 나온 문자가 유지됨
+        
         }
     }
 
